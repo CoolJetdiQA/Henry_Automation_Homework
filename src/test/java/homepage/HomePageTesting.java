d06c7d1dcf72ae3a3ca25ae023abe0ca1ba8acaf
Henry Leu <mingjenleu@gmail.com>
2022-02-25 00:04:54 -0500

    Home Page - Test case #7 to #9

--- src/test/java/homepage/HomePageTesting.java
+++ src/test/java/homepage/HomePageTesting.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -18,7 +19,7 @@ import Utilities.Utility;
 public class HomePageTesting {
 	private WebDriver driver;
 	private WebDriverWait wait;
-
+	
 	@Before
 	public void beforeTest() {
 		driver = Driver.getDriver();
@@ -280,32 +281,273 @@ public class HomePageTesting {
 		Assert.assertTrue(driver.findElement(By.xpath(Utility.getProperties("addBtn"))).isDisplayed());
 
 		// Enter quantity in the field.
-		driver.findElement(By.xpath(Utility.getProperties("qty"))).clear();
-		;
-		driver.findElement(By.xpath(Utility.getProperties("qty"))).sendKeys(Utility.getProperties("bookQty"));
+		driver.findElement(By.xpath(Utility.getProperties("qty"))).clear();		
+		driver.findElement(By.xpath(Utility.getProperties("qty"))).sendKeys(Utility.getProperties("orderQty"));
 
 		// Click "Add to Basket" button.
 		driver.findElement(By.xpath(Utility.getProperties("addBtn"))).click();
 
-		// Assert the added book quantity on the Menu item.
-		String actualItemMenu = driver.findElement(By.xpath(Utility.getProperties("itemMenu"))).getText().toUpperCase();
-		String expectedItemMenu = Utility.getProperties("bookQty");
-		expectedItemMenu += Integer.parseInt(expectedItemMenu) > 1 ? " items" : " item";
-		expectedItemMenu = expectedItemMenu.toUpperCase();
-		Assert.assertTrue(actualItemMenu.contains(expectedItemMenu));
+		// Assert the book quantity on the Menu.
+		String qtyMenu = driver.findElement(By.xpath(Utility.getProperties("qtyMenu"))).getText().toUpperCase().split("\\D")[0];
+		String orderQty = Utility.getProperties("orderQty");	
+		Assert.assertTrue(qtyMenu.contains(orderQty));
 
 		// Assert the total price on the Menu item.
 		double unitPrice = Double.parseDouble(driver.findElement(By.xpath(Utility.getProperties("unitPrice"))).getText()
 				.replaceFirst("\\S", "").replaceAll("[$,]", "").trim());
 
-		int bookQty = Integer.parseInt(Utility.getProperties("bookQty"));
-		double expectedTotalPrice = unitPrice * bookQty;
+		double expectedTotalPrice = unitPrice * Integer.parseInt(Utility.getProperties("orderQty"));
 
 		double actualTotalPrice = Double.parseDouble(driver.findElement(By.xpath(Utility.getProperties("totalPrice")))
 				.getText().replaceFirst("\\S", "").replaceAll("[$,]", "").trim());
 
 		Assert.assertEquals(Double.valueOf(expectedTotalPrice), Double.valueOf(actualTotalPrice));
 
+		System.out.println("PASSED!");
+	}
+
+	@Test
+	public void test7() {
+		System.out.print("Home Page - Test case #7 starts.....");
+
+		wait = new WebDriverWait(driver, 10);
+
+		// Click on Shop menu.
+		driver.findElement(By.id(Utility.getProperties("shop"))).click();
+
+		// Wait for the next page to load.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Utility.getProperties("home"))));
+
+		// Click on Home menu.
+		driver.findElement(By.xpath(Utility.getProperties("home"))).click();
+
+		// Wait for next page to load.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Utility.getProperties("books"))));
+
+		// List of book arrivals.
+		int actualBookArrival = driver.findElements(By.xpath(Utility.getProperties("books"))).size();
+		int expectedArrivalQty = 3;
+
+		// Assert 3 arrivals only.
+		Assert.assertTrue(actualBookArrival == expectedArrivalQty);
+
+		// Click on the book that is in stock.
+		boolean isInStock = false;
+		List<WebElement> books = driver.findElements(By.xpath(Utility.getProperties("books")));
+		for (int i = 0; i < books.size(); i++)
+			if (books.get(i).getText().toUpperCase().contains("ADD TO BASKET")) {
+				books.get(i).click();
+				isInStock = true;
+			}
+
+		// Assert book in stock.
+		Assert.assertTrue(isInStock);
+
+		// Wait for next page.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Utility.getProperties("addBtn"))));
+
+		// Assert "Add To Basket" button is visible.
+		Assert.assertTrue(driver.findElement(By.xpath(Utility.getProperties("addBtn"))).isDisplayed());
+
+		// Enter quantity in the field.
+		driver.findElement(By.xpath(Utility.getProperties("qty"))).clear();		
+		driver.findElement(By.xpath(Utility.getProperties("qty"))).sendKeys(Utility.getProperties("orderQty"));
+
+		// Click "Add to Basket" button.
+		driver.findElement(By.xpath(Utility.getProperties("addBtn"))).click();
+
+		// Assert the book quantity on the Menu.
+		String qtyMenu = driver.findElement(By.xpath(Utility.getProperties("qtyMenu"))).getText().toUpperCase().split("\\D")[0];
+		String orderQty = Utility.getProperties("orderQty");	
+		Assert.assertTrue(qtyMenu.contains(orderQty));
+
+		// Assert the total price on the Menu item.
+		double unitPrice = Double.parseDouble(driver.findElement(By.xpath(Utility.getProperties("unitPrice"))).getText()
+				.replaceFirst("\\S", "").replaceAll("[$,]", "").trim());
+
+		double expectedTotalPrice = unitPrice * Integer.parseInt(Utility.getProperties("orderQty"));
+
+		double actualTotalPrice = Double.parseDouble(driver.findElement(By.xpath(Utility.getProperties("totalPrice")))
+				.getText().replaceFirst("\\S", "").replaceAll("[$,]", "").trim());
+
+		Assert.assertEquals(Double.valueOf(expectedTotalPrice), Double.valueOf(actualTotalPrice));
+		
+		// Assert over-stock quantity
+		String inStockQtyStr = driver.findElement(By.xpath(Utility.getProperties("inStockQty"))).getText();
+		int inStockQty = Integer.parseInt(inStockQtyStr.split("\\D")[0]);
+		int overStockQty = Integer.parseInt(Utility.getProperties("overStockQty"));
+		//Assert.assertTrue(overStockQty < inStockQty) Alternative way to assert. This will fail the test.
+		if(overStockQty > inStockQty)  
+			System.out.println("Warning! Quantity entered exceeds in stock quantity.");
+		
+		System.out.println("PASSED!");
+	}
+
+	@Test
+	public void test8() {
+		System.out.print("Home Page - Test case #8 starts.....");
+
+		wait = new WebDriverWait(driver, 10);
+
+		// Click on Shop menu.
+		driver.findElement(By.id(Utility.getProperties("shop"))).click();
+
+		// Wait for the next page to load.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Utility.getProperties("home"))));
+
+		// Click on Home menu.
+		driver.findElement(By.xpath(Utility.getProperties("home"))).click();
+
+		// Wait for next page to load.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Utility.getProperties("books"))));
+
+		// List of book arrivals.
+		int actualBookArrival = driver.findElements(By.xpath(Utility.getProperties("books"))).size();
+		int expectedArrivalQty = 3;
+
+		// Assert 3 arrivals only.
+		Assert.assertTrue(actualBookArrival == expectedArrivalQty);
+
+		// Click on the book that is in stock.
+		boolean isInStock = false;
+		List<WebElement> books = driver.findElements(By.xpath(Utility.getProperties("books")));
+		for (int i = 0; i < books.size(); i++)
+			if (books.get(i).getText().toUpperCase().contains("ADD TO BASKET")) {
+				books.get(i).click();
+				isInStock = true;
+			}
+
+		// Assert book in stock.
+		Assert.assertTrue(isInStock);
+
+		// Wait for next page.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Utility.getProperties("addBtn"))));
+
+		// Assert "Add To Basket" button is visible.
+		Assert.assertTrue(driver.findElement(By.xpath(Utility.getProperties("addBtn"))).isDisplayed());
+
+		// Enter quantity in the field.
+		driver.findElement(By.xpath(Utility.getProperties("qty"))).clear();		
+		driver.findElement(By.xpath(Utility.getProperties("qty"))).sendKeys(Utility.getProperties("orderQty"));
+
+		// Click "Add to Basket" button.
+		driver.findElement(By.xpath(Utility.getProperties("addBtn"))).click();
+
+		// Assert the book quantity on the Menu.
+		String qtyMenu = driver.findElement(By.xpath(Utility.getProperties("qtyMenu"))).getText().toUpperCase().split("\\D")[0];
+		String orderQty = Utility.getProperties("orderQty");	
+		Assert.assertTrue(qtyMenu.contains(orderQty));
+
+		// Assert the total price on the Menu item.
+		double unitPrice = Double.parseDouble(driver.findElement(By.xpath(Utility.getProperties("unitPrice"))).getText()
+				.replaceFirst("\\S", "").replaceAll("[$,]", "").trim());
+
+		double expectedTotalPrice = unitPrice * Integer.parseInt(Utility.getProperties("orderQty"));
+
+		double actualTotalPrice = Double.parseDouble(driver.findElement(By.xpath(Utility.getProperties("totalPrice")))
+				.getText().replaceFirst("\\S", "").replaceAll("[$,]", "").trim());
+
+		Assert.assertEquals(Double.valueOf(expectedTotalPrice), Double.valueOf(actualTotalPrice));
+		
+		// Click on Item link and go to check out page.
+		driver.findElement(By.xpath(Utility.getProperties("qtyMenu"))).click();
+		
+		// Wait for next page.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Utility.getProperties("bookTotalTXT"))));
+				
+		// Assert "Basket Totals" is visible.
+		Assert.assertTrue(driver.findElement(By.xpath(Utility.getProperties("bookTotalTXT"))).isDisplayed());
+		
+		System.out.println("PASSED!");
+	}
+
+	@Test
+	public void test9() {
+		System.out.print("Home Page - Test case #9 starts.....");
+
+		wait = new WebDriverWait(driver, 10);
+
+		// Click on Shop menu.
+		driver.findElement(By.id(Utility.getProperties("shop"))).click();
+
+		// Wait for the next page to load.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Utility.getProperties("home"))));
+
+		// Click on Home menu.
+		driver.findElement(By.xpath(Utility.getProperties("home"))).click();
+
+		// Wait for next page to load.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Utility.getProperties("books"))));
+
+		// List of book arrivals.
+		int actualBookArrival = driver.findElements(By.xpath(Utility.getProperties("books"))).size();
+		int expectedArrivalQty = 3;
+
+		// Assert 3 arrivals only.
+		Assert.assertTrue(actualBookArrival == expectedArrivalQty);
+
+		// Click on the book that is in stock.
+		boolean isInStock = false;
+		List<WebElement> books = driver.findElements(By.xpath(Utility.getProperties("books")));
+		for (int i = 0; i < books.size(); i++)
+			if (books.get(i).getText().toUpperCase().contains("ADD TO BASKET")) {
+				books.get(i).click();
+				isInStock = true;
+			}
+
+		// Assert book in stock.
+		Assert.assertTrue(isInStock);
+
+		// Wait for next page.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Utility.getProperties("addBtn"))));
+
+		// Assert "Add To Basket" button is visible.
+		Assert.assertTrue(driver.findElement(By.xpath(Utility.getProperties("addBtn"))).isDisplayed());
+
+		// Enter quantity in the field.
+		driver.findElement(By.xpath(Utility.getProperties("qty"))).clear();		
+		driver.findElement(By.xpath(Utility.getProperties("qty"))).sendKeys(Utility.getProperties("orderQty"));
+
+		// Click "Add to Basket" button.
+		driver.findElement(By.xpath(Utility.getProperties("addBtn"))).click();
+
+		// Assert the book quantity on the Menu.
+		String qtyMenu = driver.findElement(By.xpath(Utility.getProperties("qtyMenu"))).getText().toUpperCase().split("\\D")[0];
+		String orderQty = Utility.getProperties("orderQty");	
+		Assert.assertTrue(qtyMenu.contains(orderQty));
+
+		// Assert the total price on the Menu item.
+		double unitPrice = Double.parseDouble(driver.findElement(By.xpath(Utility.getProperties("unitPrice"))).getText()
+				.replaceFirst("\\S", "").replaceAll("[$,]", "").trim());
+
+		double expectedTotalPrice = unitPrice * Integer.parseInt(Utility.getProperties("orderQty"));
+
+		double actualTotalPrice = Double.parseDouble(driver.findElement(By.xpath(Utility.getProperties("totalPrice")))
+				.getText().replaceFirst("\\S", "").replaceAll("[$,]", "").trim());
+
+		Assert.assertEquals(Double.valueOf(expectedTotalPrice), Double.valueOf(actualTotalPrice));
+		
+		// Click on Item link and go to check out page.
+		driver.findElement(By.xpath(Utility.getProperties("qtyMenu"))).click();
+		
+		// Wait for next page.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Utility.getProperties("bookTotalTXT"))));
+		
+		// Assert "Basket Totals" is visible.
+		Assert.assertTrue(driver.findElement(By.xpath(Utility.getProperties("bookTotalTXT"))).isDisplayed());
+		
+		// Enter coupon code.
+		driver.findElement(By.xpath(Utility.getProperties("couponCodeField"))).sendKeys(Utility.getProperties("couponCode"), Keys.ENTER);
+		
+		// Assert coupon discount amount.
+		double actualCouponAmount = Double.parseDouble(driver.findElement(By.xpath(Utility.getProperties("couponAmount"))).getText()
+				.replaceFirst("\\S", "").replaceAll("[$,]", "").trim());
+		
+		double expectedCouponAmount = Double.parseDouble(Utility.getProperties("expectedCouponAmount")
+				.replaceFirst("\\S", "").replaceAll("[$,]", "").trim());
+
+		Assert.assertEquals(Double.valueOf(expectedCouponAmount), Double.valueOf(actualCouponAmount));
+		
 		System.out.println("PASSED!");
 	}
 }
